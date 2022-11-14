1612745a782e01d119f19d8d90385d8e6a122782
guxukai <gxk@33.cn>
2022-11-14 10:22:53 +0800

    wip: ...;

--- packages/create-ui/templates/vue/base/vite.config.lib.js
+++ packages/create-ui/templates/vue/base/vite.config.lib.js
@@ -4,15 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import pkg from './package.json'
+import ViteConfig from './vite.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  ...ViteConfig,
   build: {
     outDir: 'dist',
     lib: {
