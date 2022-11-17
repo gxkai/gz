264f6689307d94257ee81d17851757a1de5d8dba
guxukai <gxk@33.cn>
2022-11-17 15:34:22 +0800

    wip: ...;

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [vue(), Unocss(), vueJsx(), DefineOptions()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
